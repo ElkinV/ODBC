e0f8a7c138f900e4956d7183e7ae5dd8f2c4f82c
ElkinXD <elkindavidvasquezrivera801@gmail.com>
2023-08-20 10:13:42 -0500

    añadiendo scripts backend

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ from fastapi.middleware.cors import CORSMiddleware
 
 main=FastAPI(title='CalidadAguaAPI')
 
-origins = ["*"]
-main.add_middleware(CORSMiddleware, allow_origins=origins,
+
+main.add_middleware(CORSMiddleware, allow_origins=["*"],
                    allow_credentials=True,
                    allow_methods=["*"],
                    allow_headers=["*"])
 
 
 main.include_router(router)  
+
 
 
 
